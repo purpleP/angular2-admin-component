812aca32986ed12f0f0575771fe40aff19e96a1e
Mikhail.Doronin <mikhail.doronin@moex.com>
2016-02-08 17:54:33 +0300

    can not use attribute in component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,28 +1,42 @@
-import {Component} from 'angular2/core';
-import {Http} from 'angular2/http';
+import {Component, Input, OnInit} from 'angular2/core';
+import {Http, HTTP_PROVIDERS} from 'angular2/http';
 import {CORE_DIRECTIVES} from 'angular2/angular2';
+import {ItemsPipe} from './pipe';
 
 @Component({
     selector: 'json-schema-form',
+    viewProviders: [HTTP_PROVIDERS],
     templateUrl: 'app/json-schema-form.html',
+    pipes: [ItemsPipe],
 })
 
-export class AppComponent {
-    constructor(http: Http) {
-        console.log('lalal');
-    };
+export class AppComponent implements OnInit{
+
+    constructor(http_: Http) {
+        console.log('constructor invoked');
+        this.http = http_;
+        console.log(this.http);
+        console.log(this.url);
+    }
+    ngOnInit() {
+        console.log('init invoked');
+        console.log(this.url);
+        this.http.get(this.url)
+        .subscribe(resp => this.schema = resp.json());
+    }
+
+    @Input() url;
+    http = {};
+
     schema = {
         properties: {
             id: {
                 type: 'number',
                 title: 'Resource id',
-            },
-            name: {
-                type: 'string',
-                title: 'Some name',
             }
-        }
+        },
     };
+    entity = {};
     toInputType = function(schemaType) {
         var mapping = {
             string: 'text',
